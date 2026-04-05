4c6157e2c0a50e0df9bfd7578408736c51f8cca1
Kutoux <bobby.king@umbc.edu>
2026-04-05 02:44:25 -0400

    Starting to work on frontend

---
+++ frontend/foodRecipesProject/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function Home(){
+    return(
+    <>
+        <section className='home'>
+            <div className='left'>
+                <h1>Food Recipe</h1>
+                <h5>Ongoing food recipe application to store favorite recipes either created or found.</h5>
+                <button>Share your recipe!</button>
+            </div>
+            <div className='right'></div>
+        </section>
+    </>
+  )
+}
